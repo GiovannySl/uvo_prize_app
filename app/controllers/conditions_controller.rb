f3a1f99ef653d01b25f0277895c8d4dd3e038270
Giovanny Salazar <Salazarl.giovanny@gmail.com>
2017-10-09 00:46:20 -0500

    Add bootstrap, Add create, edit and delete prize, Add condition and delete conditions from prizes

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,4 +1,34 @@
 class ConditionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def new
+    @prize_id = condition_edit_params["id"].to_i
+  end
+
   def create
+    @prize = Prize.find_by(id: condition_edit_params[:id].to_i)
+    @condition = @prize.conditions.new(condition_params.merge!({active: true}))
+    if (condition_params["number"].to_i > 0) && (condition_params["after_num"].to_i >= 0) && @condition.save
+      redirect_to prizes_path, flash: { success: 'The condition has been created' }
+    else
+      redirect_to new_condition_path, flash: { error: 'Invalid params' }
+    end
+  end
+
+  def delete
+    @condition = Condition.find_by(id: condition_edit_params[:id].to_i)
+    if @condition.delete
+      redirect_to prizes_path, flash: { alert: 'The condition has been deleted' }
+    else
+      redirect_to prizes_path, flash: { error: 'The condition could not been deleted' }
+    end
+  end
+
+  private
+  def condition_params
+    params.permit(:number, :after_num, :condition_type)
+  end
+  def condition_edit_params
+    params.permit(:id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  get 'conditions/create'
+  get 'conditions/:id/new',     to: 'conditions#new',   as: :new_condition
+  post 'conditions/:id/create', to: 'conditions#create'
+  get 'conditions/:id/edit',    to: 'conditions#edit',  as: :edit_conditions
+  post 'conditions/:id/update', to: 'conditions#update'
+  get 'conditions/:id/delete',  to: 'conditions#delete'
 
-  get 'prizes/',to: 'prizes#index', as: :prizes
-  post 'prizes/', to: 'prizes#create'
-  get 'prizes/new', to: 'prizes#new',   as: :new_post
-  post 'prizes/edit', to: 'prizes#edit'
-  delete 'prizes/delete', to: 'prizes#delete'
+  get 'prizes/',            to: 'prizes#index', as: :prizes
+  get 'prizes/new',         to: 'prizes#new',   as: :new_prize
+  post 'prizes/',           to: 'prizes#create',as: :create_prize
+  get 'prizes/:id/edit',    to: 'prizes#edit',  as: :edit_prize
+  post 'prizes/:id/update', to: 'prizes#update'
+  get 'prizes/:id/delete',  to: 'prizes#delete'
 
+  root 'prizes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
